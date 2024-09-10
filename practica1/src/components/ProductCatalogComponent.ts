c8dec5679c7ceae6ec3b2b123d26f026b85c8b18
vumg <ab17100222@gmail.com>
2024-09-10 20:36:06 +0000

    rick añadio esto

---
+++ practica1/src/components/ProductCatalogComponent.ts
@@ -0,0 +1,48 @@
+// component 
+
+import{Componen.OnInit}from'@angular/core'
+import { ProductCatalogService } from '../service/ProductCatalogService'
+@Component({
+    selector:'app-product-catalog'
+    templateUrl:'./product-catalog.component.html'
+    stleYrls:['./']
+
+    constructor(private productService:ProductCatalogService){}
+    ngOnInit():void{
+        this.productService.getProducts().subscribe((dat)=>
+        this.product = data;
+    )};
+    }
+})
+
+//metodo modificar metodo
+
+modifyProductPrice(id;string,newPrice:string){
+    const product = this.products.find(p=>p.id===id);
+    if(product){
+        product.price=newPrice;
+    }
+}
+
+//Product-catalog.html
+<h2>Catalog</h2>
+<table>
+<thead>
+<tr>
+<th>ID</td>
+<th>Nombre</td>
+<th>Precio</td>
+<th>Acciones</td>
+<hoody>
+<tr *ngFor="let product of products">
+<td>{{ProductCatalogService.id}}</td>
+<td>{{ProductCatalogService.name}}</td>
+<td>{{ProductCatalogService.price}}</td>
+<td>
+<button Click="modifyProductPrice(product.id
+45.00)">
+ModificarPrecio</button>
+</td>
+</tr>
+</tbody>
+</table>
